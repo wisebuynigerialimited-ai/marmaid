621306941bba5895b68fb0a286af2657d11eb3e8
wisebuynigerialimited-ai <wisebuynigerialimited@gmail.com>
2025-08-28 04:22:17 -0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,19 @@
+
+body{
+  background-color:white;
+}
+   button{
+     background-color:red;
+}
+     header{
+   background-color:yellow;
+    text-align:center;
+       
+}
+main{
+ text-align:center;
+  background-color:orange;
+}
+  
+   
+  
